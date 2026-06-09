73ca542851280509f01d8809ed3f13510c566eed
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-06-09 17:24:42 +0530

    update

---
+++ Day2/client/src/App.jsx
@@ -0,0 +1,7 @@
+import Task3 from "./components/Task3";
+
+function App() {
+  return <Task3 />;
+}
+
+export default App;
---
+++ Day2/client/src/components/Task3.jsx
@@ -0,0 +1,47 @@
+import  { useState } from "react";
+
+function Task3() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") return;
+
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Todo Application</h2>
+
+      <input type="text"placeholder="Enter Task"value={task}onChange={(e) => setTask(e.target.value)}/>
+
+      <button onClick={addTask}>Add Task</button>
+
+      <h3>Total Tasks: {tasks.length}</h3>
+
+      {tasks.length === 0 ? (
+        <p>No Tasks Available</p>
+      ) : (
+        <ul>
+          {tasks.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => deleteTask(index)}style={{ marginLeft: "10px" }}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Task3;
