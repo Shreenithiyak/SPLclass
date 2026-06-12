5c1a45d7658e3bedb93907ba3f80bb4af0fce71a
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-06-12 16:34:35 +0530

    updated

--- Day4/Task5.js
+++ Day4/Task5.js
@@ -17,3 +17,11 @@ const people = students.find(
 console.log(people);
 
 
+
+// Task 5: Student Management System (map + filter + find + reduce) 
+// const students = [ { id: 1, name: "Arun", mark: 85 }, { id: 2, name: "Karthik", mark: 45 },
+// { id: 3, name: "Vijay", mark: 92 }, { id: 4, name: "Ajay", mark: 35 } ]; Tasks: Display all students using map(). 
+// Display only passed students (mark >= 50) using filter(). Find student with id = 3 using find().
+//  Calculate total marks using reduce().
+//  Calculate average mark using reduce(). 
+// Interview Focus: Real-time company question combining all four methods.
---
+++ Day5/Task1.js
@@ -0,0 +1,12 @@
+// Task 1: Spread Operator Input
+//  const frontend = ["HTML", "CSS", "JavaScript"]; 
+//  const backend = ["Node.js", "MongoDB"]; 
+//  Task Using Spread Operator: Create a new array called fullStack.
+
+const frontend = ["HTML", "CSS", "JavaScript"]; 
+const backend = ["Node.js", "MongoDB"]; 
+const newARrray=[...frontend, ...backend]
+console.log(newARrray)
+
+
+
