ff828b09b10d5ffc26e43fcfc0e70ccd69262e78
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-06-12 17:42:57 +0530

    updated

---
+++ Day5/Task2.js
@@ -0,0 +1,10 @@
+// Task 2: Rest Parameter Task Create a function called totalMarks.
+// Requirements: Accept any number of marks Return total marks Use Rest 
+// Parameter Example totalMarks(80, 90, 70, 60);
+
+const totalmark=[80, 90, 70, 60];
+for(let i=0;i<totalmark.length;i++){
+    console.log(totalmark[i])
+}
+
+
---
+++ Day5/Task3.js
@@ -0,0 +1,10 @@
+
+// Task 3: Template Literals Input const name = "Ravi"; 
+// const role = "MERN Developer";
+//  const company = "Infosys"; Task Using Template Literals create:
+
+const name ="Ravi";
+const role="mern developer";
+const company="Infosys";
+const combine =` ${name}||${role}||${company}`;
+console.log(combine);
