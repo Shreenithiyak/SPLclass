e5b1ae362f82a55d6bd80ebb75df4d11fd009a84
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-06-16 11:29:04 +0530

    updated

--- Day5/Task8.js
+++ Day5/Task8.js
@@ -2,7 +2,11 @@
 // const userName = null; Task If value is null or undefined 
 // show: Guest User Using ?? Expected Output Guest User
 
+const userName = null;
 
+const displayName = userName ?? "Guest User";
+
+console.log(displayName);
 
 
 
